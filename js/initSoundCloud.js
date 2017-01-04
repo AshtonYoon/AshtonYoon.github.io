9c79aacbbe6ac103edcf0a7ef83e2b9ca469a3af
unknown <>
2017-01-04 15:37:54 +0900

    set player

--- js/initSoundCloud.js
+++ js/initSoundCloud.js
@@ -4,9 +4,33 @@
         setViewEventListeners();
     });
 
+    // only used for the firstclick
+    var isAfterFirstTrack = false;
+
+    // audio data
+    var audioCtx;
+    if ('webkitAudioContext' in window) {
+        audioCtx = new webkitAudioContext();
+    } else {
+        audioCtx = new window.AudioContext();
+    }
+    var buf;
+    var sound;
+    var gainNode;
+
+    // interval
+    var progressTimer;
+
     // id for songs
     var testId = 128843605;
     var CLIENT_ID = '8f474de4d1dedd5a6a4f4cbb60f4e6b8';
+
+    // time
+    var initialTimestamp = 0;
+    var pausedTimestamp = 0;
+    var pausedDuration = 0;
+    var now = 0;
+    var trackDuration = 0;
 
     // 검색 결과들을 담아줄 곳
     var arrayOfSongsData = [];
@@ -65,7 +89,7 @@
 
         //인자 : 개수, 음악 정보, 인덱스?
         displayTrackPicker(visibleFigure, arrayOfSongsData, index);
-        setSelectingTrackEventListerner();
+        //setSelectingTrackEventListerner();
     }
 
     // track picker의 dom을 관리함
@@ -258,6 +282,256 @@
     function displayArrow(aDom) {
         aDom.css({
             display: 'block'
+        });
+    }
+
+    function setSelectingTrackEventListerner() {
+        $('#track-picker>ul>li>figure').on('click', function() {
+            setPlayer($(this));
+        });
+    }
+
+    function setPlayer(aTarget) {
+
+        var target = aTarget.find('img');
+        var trackId = getElementId(target);
+        var streamUrl = getStreamUrl(trackId);
+        var targetSrc = getElementSrc(target);
+
+        var titleDom = aTarget.find('figcaption>span.track-title');
+        var artistNameDom = aTarget.find('figcaption>span.artist-name');
+        // hide
+        resetTrackPickerView();
+
+        // loading starts
+        addMask();
+
+        // main visual
+        var mainVisualUrl = getMainVisualUrl(targetSrc);
+        // convert attr to css format
+        mainVisualUrl = convertAttrToCss(mainVisualUrl);
+        var trackTitle = getElementText(titleDom);
+        var artistName = getElementText(artistNameDom);
+
+        // asynchronous method
+        var promise = getAudio(streamUrl);
+        promise.then(init);
+    }
+
+    function getElementId(aTarget) {
+        return aTarget.attr('id');
+    }
+
+    function getElementText(aTarget) {
+        return aTarget.text();
+    }
+
+    function getStreamUrl(aId) {
+        return 'https://api.soundcloud.com/tracks/' + aId + '/stream?client_id=' + CLIENT_ID;
+    }
+
+    function getElementSrc(aTarget) {
+        return aTarget.attr('src');
+    }
+
+    // picker 숨기기
+    function resetTrackPickerView() {
+        var $trackPicker = $('#track-picker');
+        $trackPicker.animate({
+            opacity: 0
+        }, 300, function() {
+            $trackPicker.css({
+                display: 'none',
+                opacity: 1
+            });
+        });
+
+        makeListContentsEmpty();
+    }
+
+    function makeListContentsEmpty() {
+        $('#track-picker').find('ul').empty();
+    }
+
+    // loading
+    function addMask() {
+        var mask = $('<div></div>');
+        mask.addClass('load-mask');
+        $('body').append(mask).hide().fadeIn(300);
+    }
+
+    function getMainVisualUrl(aSrc) {
+        return aSrc.replace('t300x300.jpg', 't500x500.jpg');
+    }
+
+    function convertAttrToCss(aUrl) {
+        return 'url(' + aUrl + ')';
+    }
+
+    function getAudio(aUrl) {
+        var deferred = $.Deferred();
+        // ajax is not capable of "array buffer"
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', aUrl, true);
+        xhr.responseType = 'arraybuffer';
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200 && (xhr.status !== 404)) {
+                audioCtx.decodeAudioData(xhr.response, function(buffer) {
+                    buf = buffer;
+                    return deferred.resolve(true);
+                });
+            }
+        };
+        xhr.onerror = function() {
+            return deferred.resolve(false);
+        };
+        xhr.send();
+        return deferred.promise();
+    }
+
+    function init(isAudioDataReady) {
+        // check whether or not the request successed
+        if (!isAudioDataReady) {
+            if (isAfterFirstTrack) {
+                resetTrackData();
+            }
+            removeMask();
+            // reset main visual
+            resetMainVisual();
+            showAlert();
+            return;
+        } else {
+            // start init
+            if (isAfterFirstTrack) {
+                resetAudioData();
+                resetTrackData();
+                //resetAudioEventListers();
+            } else {
+                changeUserState();
+                resetTrackData();
+            }
+
+            setAudio();
+            setAudioDuration();
+
+            setGain();
+            connectGain();
+            connectAudio();
+
+            setAudioEventListeners();
+
+            setTrackDuration();
+
+            // tell initial setting done
+            removeMask();
+        }
+    }
+
+    function resetTrackData() {
+        arrayOfSongsData = [];
+        currentIndexOfTrackOnTheLeft = 0;
+        totalSongsFigure = 0;
+    }
+
+    function resetMainVisual() {
+        var imgSrc = getMainVisualImgSrc();
+        var trackTitle = getLastTitle();
+        var artistName = getLastArtistName();
+    }
+
+    function getMainVisualImgSrc() {
+        return lastMainVisualSrc;
+    }
+
+    function getLastTitle() {
+        return lastTitle;
+    }
+
+    function getLastArtistName() {
+        return lastArtistName;
+    }
+
+    function showAlert() {
+        var msg = $('<div></div>');
+        msg.html('Not streamable. Try another song.');
+        msg.addClass('alert');
+        msg.appendTo('body')
+            .hide()
+            .fadeIn(400)
+            .fadeOut(700, function() {
+                $(this).remove();
+            });
+    }
+
+    function resetAudioData() {
+        resetData();
+        resetFaderPosition();
+        clearInterval(progressTimer);
+        disconnectAudio();
+        setStreamingState(false);
+        setModifiedPlaybackTimeStatus(false);
+    }
+
+    function resetData() {
+        setInitialPauseState(true);
+        pausedDuration = 0;
+    }
+
+    function resetFaderPosition() {
+        faderPosition = 0;
+    }
+
+    function resetAudioEventListers() {
+        $('#play-btn').off('click');
+        $('#track-bar-container').off('click');
+        $('#track-bar-container').off('mousedown', '#fader');
+        $('#volume-container>input').off('click');
+    }
+
+    function changeUserState() {
+        isAfterFirstTrack = true;
+    }
+
+    function setAudio() {
+        sound = audioCtx.createBufferSource();
+        sound.buffer = buf;
+    }
+
+    function setAudioDuration() {
+        trackDuration = sound.buffer.duration;
+    }
+
+    function setGain() {
+        gainNode = audioCtx.createGain();
+    }
+
+    function connectGain() {
+        sound.connect(gainNode);
+    }
+
+    function connectAudio() {
+        gainNode.connect(audioCtx.destination);
+    }
+
+    function setAudioEventListeners() {
+        $('#play-button').on('click', function() {
+            setStreamController();
+        });
+    }
+
+    function setTrackDuration() {
+        var html = convertPlaybackTime(trackDuration);
+        displayPlaybackTime($('#song-duration'), html);
+    }
+
+    function displayPlaybackTime(aDom, aPlaybackTimeHtml) {
+        aDom.html(aPlaybackTimeHtml);
+    }
+
+    // loading finishes
+    function removeMask() {
+        $('.load-mask').fadeOut(500, function() {
+            $(this).remove();
         });
     }
 })(window, document, window.jQuery);
